d5d263c846e2f6841a98cac453d82bb1633ea12f
user <belimov98@gmail.com>
2023-05-11 12:50:03 +0300

    Completed homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Author tolstoy = new Author("Lev", "Tolstoy");
+        Author dostoevsky = new Author("Fedor", "Dostoevsky");
+        Book warAndPeace = new Book("War and Peace", tolstoy, 1867);
+        Book crimeAndPunishment = new Book("Crime and Punishment", dostoevsky, 1866);
 
+        warAndPeace.setYear(2025);
+        System.out.println(crimeAndPunishment);
     }
 }
